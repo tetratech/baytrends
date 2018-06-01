b2ade0b34614f2f64ff65eb3b6b4556055100c87
jharcum <jon.harcum@tetratech.com>
2018-06-01 18:24:21 -0400

    v1.0.4.9003
    * Updated impute function to include maximum likelihood estimation assuming normal and lognormal distribution

--- R/imputeCensored.R
+++ R/imputeCensored.R
@@ -5,33 +5,38 @@
 #'
 #' @param x vector of type baytrends::qw  
 #' @param imputeOption imputation method [default= "mid"], valid impute options
-#'   are "lower", "upper", "mid"
+#'   are "lower", "upper", "mid", "norm", "lnorm"
 #' @details
 #' The imputeOption values of lower, upper and mid impute the lower limit, upper limit,
 #' and midpoint between the lower and upper limit. In the context of typical water quality
 #' data, these options would be eqivalent to zero, detection limit and 1/2 detection limit
-#' substitution. 
+#' substitution. Options for substituting the normal ["norm"] or lognormal ["lnorm"] expectation
+#' can also be used.
 #' 
 #' @examples
-#' x  <- dataCensored[20:36,5]
+#' x  <- dataCensored[1:20,"tdp"]
 #' x.lower <- impute(x,'lower')
 #' x.mid   <- impute(x,'mid')
 #' x.upper <- impute(x,'upper')
+#' x.norm  <- impute(x,'norm')
+#' x.lnorm <- impute(x,'lnorm')
 #' @return vector where x is transformed into a simple numeric variable
 #' @export
 #
 impute <-function(x, imputeOption="mid") { 
   
 # ----- Change history --------------------------------------------
+# 01Jun2018: JBH: Updated impute function to include maximum likelihood
+#                 estimation assuming normal and lognormal distribution
 # 01May2018: JBH: changed function name from .impute to impute; focus
 #                 lower, mid and upper options; accomodate NAs
 # 11Oct2016: JBH: corrected documentation, switched to explicit '::'
-#              for external function calls 
+#                 for external function calls 
 
 # Initialization ####
   
   # Set valid imputeOption
-  validImpute <- c("lower", "upper", "mid")
+  validImpute <- c("lower", "upper", "mid", "uniform", "norm", "lnorm")  #21May2018
   # validImpute <- c("lower", "upper", "mid", "uniform", "norm", "lnorm")
 
 # Error traps ####
@@ -86,9 +91,19 @@ impute <-function(x, imputeOption="mid") {
   } else if (imputeOption == "uniform") {
     x2[!is.na(x)] <- stats::runif(sum(!is.na(x)), x@.Data[!is.na(x),1], x@.Data[!is.na(x),2])
   } else if (imputeOption == "norm") {
-    x2[!is.na(x)] <- .simCensored(x[!is.na(x)],distr="norm")
+    myData1 <- data.frame(x=x, left=x@.Data[,1], right=x@.Data[,2])
+    fn1     <- fitdistrplus::fitdistcens(myData1[!is.na(myData1$x),], distr="norm")
+    mu.norm <- fn1$estimate[1]
+    sd.norm <- fn1$estimate[2]
+    myData1$normExpec <- .ExpNmCens(myData1,dep="x",mu.norm,sd.norm)[,1]
+    x2 <- myData1$normExpec
   } else if (imputeOption == "lnorm") {
-    x2[!is.na(x)] <- .simCensored(x[!is.na(x)],distr="lnorm")
+    myData1  <- data.frame(x=x, left=x@.Data[,1], right=x@.Data[,2])
+    fn1      <- fitdistrplus::fitdistcens(myData1[!is.na(myData1$x),], distr="lnorm")
+    mu.lnorm <- fn1$estimate[1]
+    sd.lnorm <- fn1$estimate[2]
+    myData1$lnormExpec <- .ExpLNmCens(myData1,dep="x",mu.lnorm,sd.lnorm)[,1]
+    x2 <- myData1$lnormExpec
   } 
   
   x <- x2
