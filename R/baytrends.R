86b9442ee7638ce57b3743f2fc37eb09aafb6ed1
jharcum <jon.harcum@tetratech.com>
2018-06-12 10:43:54 -0400

    v1.0.4.9005
    udpated documentation

--- R/baytrends.R
+++ R/baytrends.R
@@ -2,15 +2,16 @@
 #'
 #' The baytrends package was developed to enable users to evaluate long-term
 #' trends in the Chesapeake Bay using a Generalized Additive Modeling (GAM)
-#' approach. This package includes features that allow for explicit modeling
-#' of left- and interval-censored data as well as monitoring data that 
-#' includes laboratory method changes.
+#' approach. The model development includes selecting a GAM structure to
+#' describe nonlinear seasonally-varying changes over time, incorporation of
+#' hydrologic variability via either a river flow or salinity, the use of an
+#' intervention to deal with method or laboratory changes suspected to impact
+#' data values, and representation of left- and interval-censored data. This
+#' approach, which is fully transferable to other systems, allows for Chesapeake
+#' Bay water quality data to be evaluated in a statistically rigorous, yet
+#' flexible way to provide insights to a range of management- and
+#' research-focused questions.
 #' 
-#' @section baytrends functions:
-#' analysisOrganizeData
-#' as.qw
-#' as.qw-method
-#' ...
 #'
 #' @docType package
 #' @name baytrends
